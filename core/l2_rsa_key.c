3e40b35af49e095c269ff58fd4c81d22b279db23
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2019-03-02 18:46:21 +0000

    Fix rsa deprecated warning
    FossilOrigin-Name: d6b4cdccb07584f67983c20167178cedc081169bcec5b744297b4a843d494473

--- core/l2_rsa_key.c
+++ core/l2_rsa_key.c
@@ -11,14 +11,26 @@ struct l2_rsa_key {
 #define L2_RSA_KEY_DEFAULT_SIZE_IN_BITS 1024
 #define L2_RSA_KEY_DEFAULT_E 65537
 
-void l2_rsa_key_build(struct l2_rsa_key* key, int key_size_in_bytes, unsigned long e)
+void l2_rsa_key_build(struct l2_rsa_key* key)
 {
-    key->rsa_key = RSA_generate_key(key_size_in_bytes, e, 0, 0);
+    const char e_char[] = "65537";
+    BIGNUM* e = BN_new();
+    BN_dec2bn(&e, e_char);
+    key->rsa_key = RSA_new();
+    RSA_generate_key_ex(
+        key->rsa_key,
+        L2_RSA_KEY_DEFAULT_SIZE_IN_BITS,
+        e,
+        NULL
+    );
+    BN_free(e);
 }
 
 void l2_rsa_key_modulus(struct l2_rsa_key* key, unsigned char* dest)
 {
-    BN_bn2bin(key->rsa_key->n, dest);
+    const BIGNUM *n;
+    RSA_get0_key(key->rsa_key, &n, NULL, NULL);
+    BN_bn2lebinpad(n, dest, BN_num_bytes(n));
 }
 
 int l2_rsa_key_size(struct l2_rsa_key* key)
