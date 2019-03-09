80a4b7eb1c6267cb89dce49e9c5a0d50819303e2
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-03-09 16:03:21 -0300

    Use unsigned char (since we are dealing with bytes) for socket strategies

--- core/l2_socket.c
+++ core/l2_socket.c
@@ -8,49 +8,49 @@
 #define L2_SOCKET_WIN_ACCEPT_ERROR 5
 
 struct l2_socket {
-  int socket;
-  struct l2_socket_strategy* strategy;
+        int socket;
+        struct l2_socket_strategy* strategy;
 };
 
 struct l2_socket_strategy {
-  int (* connect)(struct l2_socket* l2_socket);
-  int (* bind)(struct l2_socket* l2_socket, unsigned short port);
-  int (* listen)(struct l2_socket* l2_socket);
-  int (* accept)(struct l2_socket* server, struct l2_socket* client);
-  int (* receive)(struct l2_socket* l2_socket, char* buffer, int buffer_size);
-  int (* send)(struct l2_socket* l2_socket, char* buffer, int buffer_size);
+        int (* connect)(struct l2_socket* l2_socket);
+        int (* bind)(struct l2_socket* l2_socket, unsigned short port);
+        int (* listen)(struct l2_socket* l2_socket);
+        int (* accept)(struct l2_socket* server, struct l2_socket* client);
+        int (* receive)(struct l2_socket* l2_socket, unsigned char* buffer, int buffer_size);
+        int (* send)(struct l2_socket* l2_socket, unsigned char* buffer, int buffer_size);
 };
 
 int l2_socket_connect(struct l2_socket* l2_socket, struct l2_socket_strategy* socket_type)
 {
-    l2_socket->strategy = socket_type;
-    return l2_socket->strategy->connect(l2_socket);
+        l2_socket->strategy = socket_type;
+        return l2_socket->strategy->connect(l2_socket);
 }
 
 int l2_socket_bind(struct l2_socket* l2_socket, unsigned short port)
 {
-    return l2_socket->strategy->bind(l2_socket, port);
+        return l2_socket->strategy->bind(l2_socket, port);
 }
 
 int l2_socket_listen(struct l2_socket* l2_socket)
 {
-    return l2_socket->strategy->listen(l2_socket);
+        return l2_socket->strategy->listen(l2_socket);
 }
 
 int l2_socket_accept(struct l2_socket* server, struct l2_socket* client)
 {
-    client->strategy = server->strategy;
-    return server->strategy->accept(server, client);
+        client->strategy = server->strategy;
+        return server->strategy->accept(server, client);
 }
 
-int l2_socket_receive(struct l2_socket* socket, char* buffer, int buffer_size)
+int l2_socket_receive(struct l2_socket* socket, unsigned char* buffer, int buffer_size)
 {
-    return socket->strategy->receive(socket, buffer, buffer_size);
+        return socket->strategy->receive(socket, buffer, buffer_size);
 }
 
-int l2_socket_send(struct l2_socket* socket, char* buffer, int buffer_size)
+int l2_socket_send(struct l2_socket* socket, unsigned char* buffer, int buffer_size)
 {
-    return socket->strategy->send(socket, buffer, buffer_size);
+        return socket->strategy->send(socket, buffer, buffer_size);
 }
 
 #endif //L2AUTH_L2_SOCKET_C
