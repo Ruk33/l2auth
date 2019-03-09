024b141006583ccb317cf772d4f946858acb6f4b
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-03-09 16:06:03 -0300

    Return rsa key modulus in big endian

--- core/l2_rsa_key.c
+++ core/l2_rsa_key.c
@@ -5,7 +5,7 @@
 #include <openssl/rsa.h>
 
 struct l2_rsa_key {
-  RSA* rsa_key;
+        RSA* rsa_key;
 };
 
 #define L2_RSA_KEY_DEFAULT_SIZE_IN_BITS 1024
@@ -28,14 +28,14 @@ void l2_rsa_key_build(struct l2_rsa_key* key)
 
 void l2_rsa_key_modulus(struct l2_rsa_key* key, unsigned char* dest)
 {
-    const BIGNUM *n;
-    RSA_get0_key(key->rsa_key, &n, NULL, NULL);
-    BN_bn2lebinpad(n, dest, BN_num_bytes(n));
+        const BIGNUM *n;
+        RSA_get0_key(key->rsa_key, &n, NULL, NULL);
+        BN_bn2bin(n, dest);
 }
 
 int l2_rsa_key_size(struct l2_rsa_key* key)
 {
-    return RSA_size(key->rsa_key);
+        return RSA_size(key->rsa_key);
 }
 
 #endif //L2AUTH_BUILD_RSA_KEY_C
