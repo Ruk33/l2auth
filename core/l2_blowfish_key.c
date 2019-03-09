204e6030b5127bf3b380ef643e03d1f3dc84626e
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2019-03-09 19:05:10 +0000

    Format & use unsigned char in l2_blowfish_key
    FossilOrigin-Name: dcedadddf86a1a14347b39e1fec835184f1352965a4120d6aceca481a0f08a93

--- core/l2_blowfish_key.c
+++ core/l2_blowfish_key.c
@@ -7,24 +7,36 @@
 #define BLOWFISH_MAX_KEY_SIZE_LENGTH 56
 
 struct l2_blowfish_key {
-  unsigned char raw_key[BLOWFISH_MAX_KEY_SIZE_LENGTH];
-  BF_KEY key;
+        unsigned char raw_key[BLOWFISH_MAX_KEY_SIZE_LENGTH];
+        BF_KEY key;
 };
 
-void l2_blowfish_key_build(struct l2_blowfish_key* key, unsigned char* raw_key, int raw_key_size)
+void l2_blowfish_key_build(
+        struct l2_blowfish_key* key,
+        unsigned char* raw_key,
+        int raw_key_size
+)
 {
-    memcpy(key->raw_key, raw_key, raw_key_size);
-    BF_set_key(&key->key, raw_key_size, key->raw_key);
+        memcpy(key->raw_key, raw_key, raw_key_size);
+        BF_set_key(&key->key, raw_key_size, key->raw_key);
 }
 
-void l2_blowfish_encrypt(struct l2_blowfish_key* key, char* src, unsigned char* dest)
+void l2_blowfish_encrypt(
+        struct l2_blowfish_key* key,
+        unsigned char* src,
+        unsigned char* dest
+)
 {
-    BF_ecb_encrypt(src, dest, &key->key, BF_ENCRYPT);
+        BF_ecb_encrypt(src, dest, &key->key, BF_ENCRYPT);
 }
 
-void l2_blowfish_decrypt(struct l2_blowfish_key* key, char* src, unsigned char* dest)
+void l2_blowfish_decrypt(
+        struct l2_blowfish_key* key,
+        unsigned char* src,
+        unsigned char* dest
+)
 {
-    BF_ecb_encrypt(src, dest, &key->key, BF_DECRYPT);
+        BF_ecb_encrypt(src, dest, &key->key, BF_DECRYPT);
 }
 
 #endif //L2AUTH_BLOWFISH_KEY_C
