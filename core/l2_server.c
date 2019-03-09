78ca739aba82c44c190c9ad0a35f3aec90e7714c
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-03-09 16:04:20 -0300

    Format l2_server

--- core/l2_server.c
+++ core/l2_server.c
@@ -3,11 +3,15 @@
 
 #include <core/l2_socket.c>
 
-void l2_server_create(struct l2_socket *server, struct l2_socket_strategy *socket_type, unsigned short port)
+void l2_server_create(
+        struct l2_socket *server,
+        struct l2_socket_strategy *socket_type,
+        unsigned short port
+)
 {
-    l2_socket_connect(server, socket_type);
-    l2_socket_bind(server, port);
-    l2_socket_listen(server);
+        l2_socket_connect(server, socket_type);
+        l2_socket_bind(server, port);
+        l2_socket_listen(server);
 }
 
 #endif //L2AUTH_L2_SERVER_C
