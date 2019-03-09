067184da62af965c822d4cfe6747b86b295706df
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2019-03-09 19:04:42 +0000

    Small refactor in decrypt_client_request
    FossilOrigin-Name: aa2f75c13b715bb6cb39bfa2adc51090017a4e6fa1d4e502f48ca45b73247b0b

--- core/decrypt_client_request.c
+++ core/decrypt_client_request.c
@@ -4,19 +4,24 @@
 #include <core/endian.c>
 #include <core/l2_blowfish_key.c>
 
-void decrypt_client_request(struct l2_blowfish_key* key, char* src, int src_size, unsigned char* dest)
+void decrypt_client_request(
+        struct l2_blowfish_key* key,
+        unsigned char* src,
+        unsigned short src_size,
+        unsigned char* dest
+)
 {
-    for (int i = 0, max = src_size/8; i<=max; i++) {
+    for (unsigned short i = 0, max = (src_size / 8 + 1) * 8; i < max; i += 8) {
         // blowfish uses big endian, l2 sends us little endian
         // convert little endian to big endian
-        encode32be(dest+0+i*8, decode32le(src+0+i*8));
-        encode32be(dest+4+i*8, decode32le(src+4+i*8));
+        encode32be(dest + i, decode32le(src + i));
+        encode32be(dest + i + 4, decode32le(src + i + 4));
 
-        l2_blowfish_decrypt(key, dest+i*8, dest+i*8);
+        l2_blowfish_decrypt(key, dest + i, dest + i);
 
         // convert it back from big endian to little endian
-        encode32le(dest+i*8, decode32be(dest+i*8));
-        encode32le(dest+4+i*8, decode32be(dest+4+i*8));
+        encode32le(dest + i, decode32be(dest + i));
+        encode32le(dest + i + 4, decode32be(dest + i + 4));
     }
 }
 
