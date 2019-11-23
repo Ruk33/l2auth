a2d3dd2d1277968123b7cfc97ab9bc7539f368a2
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2019-11-23 01:39:07 -0300

    Add session key

---
+++ login/session_key.c
@@ -0,0 +1,26 @@
+#ifndef L2AUTH_LOGIN_SESSION_KEY_C
+#define L2AUTH_LOGIN_SESSION_KEY_C
+
+#include <stdlib.h>
+
+struct LoginSessionKey
+{
+        int playOK1;
+        int playOK2;
+        int loginOK1;
+        int loginOK2;
+};
+
+struct LoginSessionKey* login_session_key_create()
+{
+        struct LoginSessionKey* session_key = malloc(sizeof(struct LoginSessionKey));
+
+        session_key->playOK1 = rand();
+        session_key->playOK2 = rand();
+        session_key->loginOK1 = rand();
+        session_key->loginOK2 = rand();
+
+        return session_key;
+}
+
+#endif
