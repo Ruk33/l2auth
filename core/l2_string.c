90d274ed9d3c7c94b334e213a26b5e47b2e2b2f3
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2020-04-17 23:20:38 +0000

    Add l2 string
    FossilOrigin-Name: 4c7aa1b711c4a7765dc310fde7aa40534c26e1c4db2b444d623835e05ff82257

---
+++ core/l2_string.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <core/l2_string.h>
+
+void l2_string_to_char(l2_string* src, char* dest, size_t size)
+{
+        assert(src);
+        assert(dest);
+
+        for(size_t i = 0; i < size; i++) {
+                dest[i] = *src;
+                src += 2;
+        }
+}
+
+void l2_string_from_char(l2_string* dest, char* src, size_t size)
+{
+        assert(dest);
+        assert(src);
+
+        for(size_t i = 0; i < size; i++) {
+                *dest = src[i];
+                *(dest + 1) = 0;
+                dest += 2;
+        }
+}
+
+void l2_string_from_l2(l2_string* dest, l2_string* src, size_t size)
+{
+        assert(dest);
+        assert(src);
+
+        for(size_t i = 0, max = size * 2; i < max; i += 2) {
+                dest[i] = src[i];
+                dest[i + 1] = src[i + 1];
+        }
+}
+
+size_t l2_string_calculate_space_from_char(size_t original_string)
+{
+        return original_string * 2;
+}
