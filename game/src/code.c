b25a66bcb833a1a2dbfd489f9431e3e992523abb
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2020-07-28 09:51:32 +0000

    Check if lib changed before loading it
    FossilOrigin-Name: ca224bd64a791c5675571b32cf098e1b0873c042d010464724f57fe01184b2da

--- game/src/code.c
+++ game/src/code.c
@@ -1,11 +1,31 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <dlfcn.h>
+#include <string.h>
 #include <log/log.h>
 #include "code.h"
 
+static time_t lib_last_modified()
+{
+        struct stat file_stat;
+        int err = stat("./build/libgameserver.so", &file_stat);
+    
+        if (err != 0) {
+                log_fatal("Error while checking if lib changed");
+                return 0;
+        }
+
+        return file_stat.st_mtime;
+}
+
 void code_load(struct Code *code)
 {
-        if (code->library) dlclose(code->library);
+        time_t llm = lib_last_modified();
 
+        if (llm <= code->last_changed) return;
+
+        if (code->library) dlclose(code->library);
         code->library = dlopen("libgameserver.so", RTLD_LAZY);
 
         if (!code->library) {
@@ -18,6 +38,7 @@ void code_load(struct Code *code)
         code->new_conn = dlsym(code->library, "entry_new_conn");
         code->handle_request = dlsym(code->library, "entry_handle_request");
         code->handle_disconnect = dlsym(code->library, "entry_handle_disconnect");
+        memcpy(&code->last_changed, &llm, sizeof(llm));
 
         if (code->new_conn && code->handle_request && code->handle_disconnect) {
                 log_info("Functions successfully loaded");
--- game/src/code.h
+++ game/src/code.h
@@ -1,11 +1,13 @@
 #ifndef CODE_H
 #define CODE_H
 
+#include <sys/types.h>
 #include "../shared/client_id.h"
 #include "../shared/l2_server.h"
 #include "../shared/request.h"
 
 struct Code {
+        time_t last_changed;
         void* library;
         void* (* new_conn)(struct L2Server *l2_server, client_id id);
         void  (* handle_request)(struct Request *request);
