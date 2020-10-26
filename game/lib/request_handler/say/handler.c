73495214c14feb2955a48348ef368385b41047ca
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2020-10-26 07:35:32 +0000

    Fix say package
    FossilOrigin-Name: f1dc1e8d5ca13fd8f356aa4761b06960fec0ec4e6ecf0cb3494be0f798dfa7b3

--- game/lib/request_handler/say/handler.c
+++ game/lib/request_handler/say/handler.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 #include <core/l2_string.h>
 #include <core/l2_raw_packet.h>
 #include "../../request.h"
@@ -15,17 +16,18 @@ void say_handler(struct Request *request)
         struct Client *client = request->client;
         l2_raw_packet *packet = request->packet;
 
-        l2_string *message = NULL;
-        l2_packet *response = NULL;
+        l2_string *l2_message = NULL;
+        char *message = NULL;
+        size_t message_size = 0;
 
-        message = (l2_string *)l2_packet_content(packet);
-        response = say_response(client, message);
+        l2_message = (l2_string *)l2_packet_content(packet);
+        message_size = l2_string_len(l2_message) + 1;
+        message = client_alloc_mem(client, message_size);
+        l2_string_to_char(l2_message, message, message_size);
 
-        // client_encrypt_packet(client, response);
-        // client_queue_response(client, response);
-        world_state_client_say(request->world_state, client, "Hi", 3);
+        world_state_client_say(request->world_state, client, message, message_size);
 
         client_update_request_handler(client, &enter_world_next_handler);
 
-        client_free_mem(client, response);
+        client_free_mem(client, message);
 }
