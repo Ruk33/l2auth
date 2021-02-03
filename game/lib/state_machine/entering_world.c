61dfd0ad23cd656d3b3f3b31583fe8db5ee5e279
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2021-02-03 11:33:24 +0000

    Refactor d0 request state machine
    FossilOrigin-Name: 4a90fa8b22c835a1cd905843ccf2f5bb1365172d306c3d8faaa6926eb68790cb

--- game/lib/state_machine/entering_world.c
+++ game/lib/state_machine/entering_world.c
@@ -1,10 +1,21 @@
 #include <request.h>
-#include <client_request/d0.h>
+#include <server_packet/d0.h>
 #include <client_request/quest_list.h>
 #include <client_request/enter_world.h>
 #include <client_packet/type.h>
 #include "in_world.h"
 #include "entering_world.h"
+
+static void d0(request_t *request)
+{
+        packet response[SERVER_PACKET_D0_FULL_SIZE] = {0};
+
+        assert_valid_request(request);
+
+        server_packet_d0(response);
+        session_encrypt_packet(request->session, response, response, (size_t) packet_get_size(response));
+        request->host->send_response(request->session->socket, response, (size_t) packet_get_size(response));
+}
 
 void state_machine_entering_world(request_t *request)
 {
@@ -16,8 +27,7 @@ void state_machine_entering_world(request_t *request)
 
         switch (type) {
         case CLIENT_PACKET_TYPE_REQUEST_AUTO_SS_BSPS:
-                client_request_d0(request->session->socket, request->session, request->host->send_response);
-                session_update_state(request->session, ENTERING_WORLD);
+                d0(request);
                 break;
         case CLIENT_PACKET_TYPE_REQUEST_QUEST_LIST:
                 client_request_quest_list(request->session->socket, request->session, request->host->send_response);
