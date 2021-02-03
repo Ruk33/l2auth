e5c911dc51eea2feddb4728f37626f294f84f59b
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2021-02-03 12:46:01 +0000

    Refactor protocol version request
    FossilOrigin-Name: 81c348144a5cfcaf11cc26c5335bfabc667847adbcd744d023bd0ba1951ce973

--- game/lib/state_machine/protocol_version.c
+++ game/lib/state_machine/protocol_version.c
@@ -1,20 +1,35 @@
 #include <request.h>
-#include <client_request/protocol_version.h>
+#include <server_packet/protocol_version.h>
 #include <client_packet/type.h>
 #include "protocol_version.h"
 
-void state_machine_protocol_version(request_t *request)
+/**
+ * When the user is switching from login to game server
+ * this is the packet to be handled.
+ */
+static void protocol_version(request_t *request)
 {
+        packet response[SERVER_PACKET_PROTOCOL_VERSION_FULL_SIZE] = {0};
+
         assert_valid_request(request);
 
+        server_packet_protocol_version(response);
+        request->host->send_response(request->session->socket, response, (size_t) packet_get_size(response));
+
+        session_update_state(request->session, AUTH_REQUEST);
+}
+
+void state_machine_protocol_version(request_t *request)
+{
         packet_type type = 0;
+
+        assert_valid_request(request);
 
         type = packet_get_type(request->packet);
 
         switch (type) {
         case CLIENT_PACKET_TYPE_PROTOCOL_VERSION:
-                client_request_protocol_version(request->session->socket, request->host->send_response);
-                session_update_state(request->session, AUTH_REQUEST);
+                protocol_version(request);
                 break;
         default:
                 printf("Packet %02X can't be handled by state_machine_protocol_version.\n", type);
