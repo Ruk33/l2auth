aa78230d461ebcef7f25d01b3ea96892ef684d63
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2021-03-05 23:34:48 +0000

    Allow to see other characters
    FossilOrigin-Name: eeac43e7c3a66aa1d3c20ba5bd94e5b4157190116ef0abbd851df0a87ded7d31

--- game/lib/state_machine/character/spawn.c
+++ game/lib/state_machine/character/spawn.c
@@ -5,18 +5,23 @@
 #include <server_packet/char_info.h>
 #include "spawn.h"
 
-static void spawn_character(request_t *request, character_t *character)
+static void spawn_character(request_t *request, character_t *spawned_character)
 {
-        assert_valid_request(request);
-        assert(character);
+        packet *char_info_packet = NULL;
 
-        packet response[PACKET_SAFE_FULL_SIZE(server_packet_char_info_t)] = {0};
         struct List *close_characters = NULL;
         struct ListEntry *i_close_character = NULL;
         character_t *close_character = NULL;
 
-        close_characters = list_create(request->host->alloc_memory, request->host->dealloc_memory);
-        storage_character_close_to(&request->storage->character_storage, &close_characters, character, 1200);
+        assert_valid_request(request);
+        assert(spawned_character);
+
+        char_info_packet = request->host->alloc_memory(
+                PACKET_SAFE_FULL_SIZE(server_packet_char_info_t));
+        close_characters = list_create(request->host->alloc_memory,
+                                       request->host->dealloc_memory);
+        storage_character_close_to(&request->storage->character_storage,
+                                   &close_characters, spawned_character, 1200);
         i_close_character = list_get_iterator(close_characters);
 
         printf("Player spawned\n");
@@ -25,21 +30,39 @@ static void spawn_character(request_t *request, character_t *character)
                 close_character = list_get_value(i_close_character);
                 i_close_character = list_get_next(i_close_character);
 
-                printf("Showing new spawned player to %d\n", close_character->id);
+                printf("Showing new spawned player to %d\n",
+                       close_character->id);
 
-                if (close_character->id == character->id) {
+                if (close_character->id == spawned_character->id) {
                         printf("Same character, ignoring\n");
                         continue;
                 }
 
-                server_packet_char_info(response, character, 0);
-                session_encrypt_packet(close_character->session, response, response, (size_t) packet_get_size(response));
-                request->host->send_response(close_character->session->socket, response, (size_t) packet_get_size(response));
+                /*
+                 * Notify close character of the player being spawn.
+                 */
+                server_packet_char_info(char_info_packet, spawned_character, 0);
+                session_encrypt_packet(
+                        close_character->session, char_info_packet,
+                        char_info_packet,
+                        (size_t) packet_get_size(char_info_packet));
+                request->host->send_response(
+                        close_character->session->socket, char_info_packet,
+                        (size_t) packet_get_size(char_info_packet));
 
-                memset(response, 0, sizeof(response));
+                /*
+                 * Also notify the spawning player of the close character.
+                 */
+                server_packet_char_info(char_info_packet, close_character, 0);
+                session_encrypt_packet(
+                        request->session, char_info_packet, char_info_packet,
+                        (size_t) packet_get_size(char_info_packet));
+                request->host->send_response(
+                        request->session->socket, char_info_packet,
+                        (size_t) packet_get_size(char_info_packet));
         }
 
-        fflush(stdout);
+        request->host->dealloc_memory(char_info_packet);
         list_free(close_characters);
 }
 
