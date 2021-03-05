5592eaa2e54e32e9e1f2383b952c32ffa8f35886
franco.montenegro.ruke@gmail.com <franco.montenegro.ruke@gmail.com@noemail.net>
2021-03-05 21:37:03 +0000

    Add clang format and improve socket behavior
    FossilOrigin-Name: 543e61d832547f5cba0b20390045e6fd73f03c27ff9724328248ecddffb0aa08

--- game/socket.c
+++ game/socket.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
@@ -11,20 +13,55 @@
 #include "memory.h"
 #include "socket.h"
 
-#define MAX_REQUESTS 10
+#define MAX_REQUESTS 100
 #define MAX_REQUEST_SIZE 65536
 
-#define EXPR_OR_RETURN_ERR(expr)        \
-        do {                            \
-                if ((expr) == -1) {     \
-                        return -1;      \
-                }                       \
+#define EXPR_OR_RETURN_ERR(expr)    \
+        do {                        \
+                if ((expr) == -1) { \
+                        return -1;  \
+                }                   \
         } while (0)
+
+struct Client {
+        int active;
+        int fd;
+        unsigned char *read_buffer;
+        unsigned char *write_buffer;
+        size_t written_bytes;
+        size_t to_be_written;
+};
+
+static struct Client *clients = NULL;
+static struct epoll_event *events = NULL;
+
+static void init_client(struct Client *client, int fd)
+{
+        assert(client);
+
+        client->fd = fd;
+        client->active = 1;
+        client->read_buffer = memory_alloc(MAX_REQUEST_SIZE);
+        client->write_buffer = memory_alloc(MAX_REQUEST_SIZE);
+        client->written_bytes = 0;
+        client->to_be_written = 0;
+}
+
+static struct Client *client_by_fd(int fd)
+{
+        for (int i = 0; i < MAX_REQUESTS; i++) {
+                if (clients[i].fd == fd) {
+                        return &clients[i];
+                }
+        }
+
+        return NULL;
+}
 
 int socket_create(unsigned short port, size_t max_connections)
 {
         int server = 0;
-        struct sockaddr_in address = {0};
+        struct sockaddr_in address = { 0 };
 
         assert(port);
         assert(max_connections);
@@ -35,7 +72,8 @@ int socket_create(unsigned short port, size_t max_connections)
         address.sin_addr.s_addr = INADDR_ANY;
         address.sin_port = htons(port);
 
-        EXPR_OR_RETURN_ERR(bind(server, (struct sockaddr *)&address, sizeof(address)));
+        EXPR_OR_RETURN_ERR(
+                bind(server, (struct sockaddr *) &address, sizeof(address)));
         EXPR_OR_RETURN_ERR(listen(server, max_connections));
 
         return server;
@@ -43,121 +81,119 @@ int socket_create(unsigned short port, size_t max_connections)
 
 int socket_accept(int fd)
 {
-        struct sockaddr_in address = {0};
+        struct sockaddr_in address = { 0 };
         unsigned int addrlen = 0;
-
         return accept(fd, (struct sockaddr *) &address, &addrlen);
 }
 
 ssize_t socket_send(int fd, unsigned char *response, size_t response_size)
 {
-        ssize_t sent = 0;
-        ssize_t tmp = 0;
+        struct Client *client = NULL;
+        ssize_t sent_bytes = 0;
+        int can_not_write = 0;
 
-        if (!fd || !response || !response_size) {
+        if (!response || !response_size) {
                 return 0;
         }
 
-        /*
-         * TODO make sure we don't overflow
-         */
-        while (sent != (ssize_t) response_size) {
-                tmp = send(fd, response + sent, response_size - sent, 0);
+        client = client_by_fd(fd);
 
-                if (tmp == -1) {
-                        return -1;
-                }
-
-                sent += tmp;
+        if (!client) {
+                return -1;
         }
 
-        return sent;
+        memcpy(client->write_buffer + client->to_be_written, response,
+               response_size);
+        client->to_be_written += response_size;
+
+        sent_bytes = send(fd, client->write_buffer + client->written_bytes,
+                          client->to_be_written - client->written_bytes, 0);
+        can_not_write = sent_bytes < 0 &&
+                        (errno == EWOULDBLOCK || errno == EAGAIN);
+
+        if (can_not_write) {
+        } else if (sent_bytes > 0) {
+                client->written_bytes += sent_bytes;
+                if (client->written_bytes == client->to_be_written) {
+                        client->written_bytes = 0;
+                        client->to_be_written = 0;
+                }
+        }
+
+        return sent_bytes;
 }
 
 void socket_close(int fd)
 {
+        struct Client *client = NULL;
+
         close(fd);
+
+        client = client_by_fd(fd);
+
+        if (!client) {
+                return;
+        }
+
+        client->active = 0;
+        memory_free(client->read_buffer);
+        memory_free(client->write_buffer);
 }
 
 static void socket_set_non_blocking(int fd)
 {
         int flags = 0;
-
-// If they have O_NONBLOCK, use the Posix way to do it
-#if defined(O_NONBLOCK)
-        if (-1 == (flags = fcntl(fd, F_GETFL, 0)))
+        if ((flags = fcntl(fd, F_GETFL, 0)) == -1) {
                 flags = 0;
+        }
         fcntl(fd, F_SETFL, flags | O_NONBLOCK);
-#else
-        // Otherwise, use the old way of doing it
-        flags = 1;
-        ioctl(fd, FIOBIO, &flags);
-#endif
 }
 
 static int socket_handle_conn(socket_conn_t *conn, int epoll_fd)
 {
-        struct epoll_event client_event = {0};
+        size_t free_client_index = 0;
+        struct Client *client = NULL;
+        struct epoll_event client_event = { 0 };
+        int client_fd = 0;
 
-        client_event.events = EPOLLIN | EPOLLET;
-        client_event.data.fd = conn->on_connect(conn->fd, conn->data);
+        client_fd = conn->on_connect(conn->fd, conn->data);
+        EXPR_OR_RETURN_ERR(client_fd);
+        socket_set_non_blocking(client_fd);
 
-        EXPR_OR_RETURN_ERR(client_event.data.fd);
+        /*
+         * Find a free client to be used.
+         */
+        do {
+                client = &clients[free_client_index];
+                free_client_index += 1;
+        } while (free_client_index < MAX_REQUESTS && client->active);
 
-        socket_set_non_blocking(client_event.data.fd);
+        init_client(client, client_fd);
+
+        client_event.events = EPOLLIN;
+        client_event.data.fd = client_fd;
 
         epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_event.data.fd, &client_event);
 
         return 0;
 }
 
-static ssize_t socket_receive(int fd, unsigned char *dest, size_t dest_size)
-{
-        ssize_t read = 0;
-        ssize_t tmp = 0;
-
-        assert(dest);
-
-        /*
-         * TODO make sure we don't overflow
-         */
-        while (1) {
-                tmp = recv(fd, dest + read, dest_size - read, 0);
-
-                if (read > 0 && tmp == -1) {
-                        return read;
-                }
-
-                if (tmp == -1) {
-                        return -1;
-                }
-
-                if (tmp == 0) {
-                        break;
-                }
-
-                read += tmp;
-        }
-
-        return read;
-}
-
 static int socket_handle_events(socket_conn_t *conn, int epoll_fd)
 {
         int events_count = 0;
-        struct epoll_event events[MAX_REQUESTS] = {0};
 
         int fd = 0;
         int is_new_conn = 0;
+        struct Client *client = NULL;
+        ssize_t tmp = 0;
 
-        unsigned char *request = 0;
-        ssize_t request_size = 0;
+        int can_not_read = 0;
+        int can_not_write = 0;
 
         assert(conn);
 
-        request = memory_alloc(MAX_REQUEST_SIZE);
-
-        EXPR_OR_RETURN_ERR(events_count = epoll_wait(epoll_fd, events, MAX_REQUESTS, -1));
+        EXPR_OR_RETURN_ERR(
+                events_count = epoll_wait(epoll_fd, events, MAX_REQUESTS, -1));
 
         for (int i = 0; i < events_count; i++) {
                 fd = events[i].data.fd;
@@ -168,42 +204,82 @@ static int socket_handle_events(socket_conn_t *conn, int epoll_fd)
                         continue;
                 }
 
-                memset(request, 0, MAX_REQUEST_SIZE);
-                request_size = socket_receive(fd, request, MAX_REQUEST_SIZE);
-                printf("Socket packet: %ld\n", request_size);
+                client = client_by_fd(fd);
 
-                if (request_size == 0) {
-                        conn->on_disconnect(fd, conn->data);
-                        epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+                if (!client) {
                         continue;
                 }
 
-                conn->on_request(fd, conn->data, request, request_size);
-        }
+                /*
+                 * Read
+                 */
+                if (events[i].events & EPOLLIN) {
+                        memset(client->read_buffer, 0, MAX_REQUEST_SIZE);
 
-        memory_free(request);
+                        tmp = recv(fd, client->read_buffer, MAX_REQUEST_SIZE,
+                                   0);
+                        can_not_read = tmp < 0 && (errno == EAGAIN ||
+                                                   errno == EWOULDBLOCK);
+
+                        if (can_not_read) {
+                                // Not sure if this could ever happen?
+                        } else if (tmp > 0) {
+                                conn->on_request(fd, conn->data,
+                                                 client->read_buffer, tmp);
+                        } else {
+                                conn->on_disconnect(fd, conn->data);
+                                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL);
+                        }
+                }
+
+                /*
+                 * Write
+                 */
+                if (events[i].events & EPOLLOUT) {
+                        tmp = send(fd,
+                                   client->write_buffer + client->written_bytes,
+                                   client->to_be_written -
+                                           client->written_bytes,
+                                   0);
+                        can_not_write = tmp < 0 && (errno == EAGAIN ||
+                                                    errno == EWOULDBLOCK);
+
+                        if (can_not_write) {
+                                // Not sure if this could ever happen?
+                        } else if (tmp > 0) {
+                                client->written_bytes += tmp;
+                                if (client->written_bytes ==
+                                    client->to_be_written) {
+                                        client->written_bytes = 0;
+                                        client->to_be_written = 0;
+                                }
+                        }
+                }
+        }
 
         return 0;
 }
 
 int socket_handle_requests(socket_conn_t *conn)
 {
-        int epoll_fd = 0;
-        struct epoll_event event = {0};
+        int epollfd = 0;
+        struct epoll_event event = { 0 };
 
         assert(conn);
 
         socket_set_non_blocking(conn->fd);
-
-        EXPR_OR_RETURN_ERR(epoll_fd = epoll_create1(0));
+        EXPR_OR_RETURN_ERR(epollfd = epoll_create1(0));
 
         event.events = EPOLLIN;
         event.data.fd = conn->fd;
 
-        EXPR_OR_RETURN_ERR(epoll_ctl(epoll_fd, EPOLL_CTL_ADD, conn->fd, &event));
+        EXPR_OR_RETURN_ERR(epoll_ctl(epollfd, EPOLL_CTL_ADD, conn->fd, &event));
+
+        events = memory_alloc(sizeof(*events) * MAX_REQUESTS);
+        clients = memory_alloc(sizeof(*clients) * MAX_REQUESTS);
 
         while (1) {
-                EXPR_OR_RETURN_ERR(socket_handle_events(conn, epoll_fd));
+                EXPR_OR_RETURN_ERR(socket_handle_events(conn, epollfd));
         }
 
         return 0;
