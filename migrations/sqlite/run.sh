c639ac576e1386b2099838146241100f2d7da2fc
Franco Montenegro <franco.montenegro.ruke@gmail.com>
2021-08-24 16:19:35 -0300

    Add run migrations script.

---
+++ migrations/sqlite/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit if anything fails
+set -e
+
+# $0 = run.sh
+# $1 = Database path
+
+RUN_PATH="`dirname \"$0\"`"
+RUN_PATH="`( cd \"$RUN_PATH\" && pwd )`"
+
+for migration in $RUN_PATH/*.sql; do
+    echo "Running migration $migration"
+
+    if [[ $migration == *_.sql ]]
+    then
+        sqlite3 $1 < $migration >/dev/null 2>&1 || true
+    else
+        sqlite3 $1 < $migration
+    fi
+done
+
+sqlite3 $1 '.schema' > schema.txt
