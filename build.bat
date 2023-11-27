f0deff74b89842d7294d732b821f89734c1e102a
Franco Montenegro <franco@francomontenegro.com.ar>
2023-11-26 22:56:00 -0300

    add bin folder.

--- build.bat
+++ build.bat
@@ -2,4 +2,6 @@
 
 call vcvarsall x64 >nul
 
-cl /Zi /W4 /wd4996 /wd4201 /wd4477 /nologo -Gm- -GR- login_server.c asocket_windows.c blowfish.c -Iopenssl-1.1/x64/include openssl-1.1/x64/lib/libcrypto.lib Ws2_32.lib 
+pushd bin
+cl /Zi /W4 /wd4996 /wd4201 /wd4477 /nologo -Gm- -GR- ../login_server.c ../asocket_windows.c ../blowfish.c -I../openssl-1.1/x64/include ../openssl-1.1/x64/lib/libcrypto.lib Ws2_32.lib 
+popd
