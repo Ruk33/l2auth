a97cdf18598262d96bca5c442565f40a2cbdec4f
Franco Montenegro <franco@francomontenegro.com.ar>
2024-09-01 13:45:01 -0300

    fix linux build script.

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ gcc -Wall -Wextra -o login_server ../login_server.c ../directory.c ../net_linux.
 
 gcc -Wall -Wextra -o game_server.so -fPIC -rdynamic -shared ../game_server_lib.c ../directory.c ../net_linux.c ../wqueue.c  -lpthread -lm
 
-gcc -Wall -Wextra -o game_server ../game_server.c ../net_linux.c ../lib.c -lpthread -ldl
+gcc -Wall -Wextra -o game_server ../game_server.c ../net_linux.c ../library.c -lpthread -ldl
 
 popd
