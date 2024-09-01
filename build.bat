9968326095b7e6c286e25d9a618219a18c68b62d
Franco Montenegro <franco@francomontenegro.com.ar>
2024-09-01 19:29:21 -0300

    remove wqueue dependency from build bat as well for login server.

--- build.bat
+++ build.bat
@@ -8,7 +8,7 @@ del *.pdb >nul
 
 set flags=/Od /Z7 /W4 /wd4996 /wd4201 /wd4477 /nologo /diagnostics:column -FC -Gm- -GR-
 
-cl %flags% ../login_server.c ../directory.c ../net_windows.c ../wqueue.c /Fe:login_server.exe -I../openssl-1.1/x64/include ../openssl-1.1/x64/lib/libcrypto.lib Ws2_32.lib
+cl %flags% ../login_server.c ../directory.c ../net_windows.c /Fe:login_server.exe -I../openssl-1.1/x64/include ../openssl-1.1/x64/lib/libcrypto.lib Ws2_32.lib
 
 cl %flags% /LD ../game_server_lib.c ../directory.c ../net_windows.c ../wqueue.c /Fe:game_server.dll /link -incremental:no /PDB:game_server_%random%.pdb /EXPORT:on_pevent Ws2_32.lib
 
